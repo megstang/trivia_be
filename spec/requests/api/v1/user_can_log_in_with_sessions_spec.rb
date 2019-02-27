73313b8dfd56d7b007415fb6b7355ffb9531ee52
Meghan Stang <mstang15@gmail.com>
2019-02-26 23:37:06 -0700

    Add sad testing path for user log in

--- spec/requests/api/v1/user_can_log_in_with_sessions_spec.rb
+++ spec/requests/api/v1/user_can_log_in_with_sessions_spec.rb
@@ -21,6 +21,26 @@ RSpec.describe "Post Sessions endpoint" do
     expect(response.status).to eq(200)
   end
 
+  it "log in a user throws error if incorrect password" do
+    data_register = {
+      "name": "meg",
+      "email": "mstang15@gmail.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+
+    post "/api/v1/users", params: data_register
+
+    data_session = {
+      "email": "mstang15@gmail.com",
+      "password": "paword"
+    }
+
+    post "/api/v1/sessions", params: data_session
+
+    expect(response.status).to eq(404)
+  end
+
 
 
 end
